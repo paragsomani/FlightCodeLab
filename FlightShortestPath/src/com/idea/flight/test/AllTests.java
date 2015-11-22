49dfcec81a32a03b82537600083a89c48469a821
paragsomani <somaniparag@gmail.com>
2015-11-22 11:05:37 +0530

    Flight shortest path code lab
    First commit of complete project for flight shortest path lab. It finds
    shortest path between any two locations.

---
+++ FlightShortestPath/src/com/idea/flight/test/AllTests.java
@@ -0,0 +1,12 @@
+package com.idea.flight.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({TestShortestPath.class})
+public class AllTests {
+
+}
